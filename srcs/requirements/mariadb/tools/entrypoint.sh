54ac34c07f98f331b5fb8e6e1715d623c23eacb8
Lillian Gottschalk <lgottsch@c3r6s3.42prague.com>
2025-09-27 18:49:06 +0200

    going back to old v

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -11,34 +11,37 @@ DB_ADMIN_PW=$(cat /run/secrets/db_admin_pw)
 
 mkdir -p "$DATADIR"
 chown  -R mysql:mysql "$DATADIR" #change ownership to mariadb user
-
+chmod -R 700 "$DATADIR"
 
 # Initialize system database if missing
 if [ ! -d "$DATADIR/mysql" ]; then
     echo "Initializing system database..."
-    mariadb-install-db --user=mysql --datadir="$DATADIR"
+    mariadb-install-db --user=mysql --datadir="$DATADIR" --skip-log-bin
 fi
 
 
 # If first run (fresh install), run init.sql
 if [ ! -f "$DATADIR/.initialized" ]; then
-    echo "Initializing database with init.sql..."
+    echo "Initializing database..."
 
     mysqld --datadir="$DATADIR" --bootstrap <<-EOSQL
-CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS '${DB_NORMAL_USER}'@'%' IDENTIFIED BY '${DB_NORMAL_PW}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_NORMAL_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_NORMAL_USER}'@'%';
 
 CREATE USER IF NOT EXISTS '${DB_ADMIN}'@'%' IDENTIFIED BY '${DB_ADMIN_PW}';
 GRANT ALL PRIVILEGES ON *.* TO '${DB_ADMIN}'@'%' WITH GRANT OPTION;
 
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ADMIN_PW}';
+
 FLUSH PRIVILEGES;
 EOSQL
+
 
 	touch "$DATADIR/.initialized"
     echo "Initialization complete."
 fi
 
 
-exec mysqld_safe --datadir="$DATADIR" --bind-address=0.0.0.0
+exec /usr/bin/mysqld_safe --datadir="$DATADIR" --bind-address=0.0.0.0
